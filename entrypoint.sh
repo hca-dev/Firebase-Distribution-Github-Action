f077da44bec58fbc4cd95814644938cbcc5bfe64
Lakshmoji Rao Y <lakshmojirao999@gmail.com>
2021-03-11 09:05:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
 
-ls -lrt
-printenv
+RELEASE_NOTES=""
+RELEASE_NOTES_FILE=""
+
+if [[ -z ${INPUT_RELEASENOTES} ]]; then
+        RELEASE_NOTES="$(git log -1 --pretty=short)"
+else
+        RELEASE_NOTES=${INPUT_RELEASENOTES}
+fi
+
+if [[ ${INPUT_RELEASENOTESFILE} ]]; then
+        RELEASE_NOTES=""
+        RELEASE_NOTES_FILE=${INPUT_RELEASENOTESFILE}
+fi
+
+if [ -n "${INPUT_SERVICECREDENTIALSFILE}" ] ; then
+    export GOOGLE_APPLICATION_CREDENTIALS="${INPUT_SERVICECREDENTIALSFILE}"
+fi
+
+if [ -n "${INPUT_TOKEN}" ] ; then
+    export FIREBASE_TOKEN="${INPUT_TOKEN}"
+fi
 
 firebase \
         appdistribution:distribute \
-        "$INPUT_FILE/$INPUT_FILE" \
+        "$INPUT_FILE" \
         --app "$INPUT_APPID" \
         --groups "$INPUT_GROUPS" \
-        --release-notes "test" \
-        --token "$INPUT_TOKEN"
+        ${RELEASE_NOTES:+ --release-notes "${RELEASE_NOTES}"} \
+        ${INPUT_RELEASENOTESFILE:+ --release-notes-file "${RELEASE_NOTES_FILE}"} \
+        $( (( $INPUT_DEBUG )) && printf %s '--debug' )
+
+
